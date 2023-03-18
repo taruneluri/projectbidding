a3c2985c34cbccb12bf243872fc6107841ae04ec
taruneluri <taruneluri2106@gmail.com>
2023-03-18 11:27:18 +0530

    cost estimation done

--- controllers/costestimation.js
+++ controllers/costestimation.js
@@ -5,10 +5,21 @@ router.use(express.json());
 router.use(express.static('pages'));
 router.use(express.urlencoded({extended:false}));
 var mongoose=require('./mongodb');
+var Csmodel= require('../schema/csmodel');
 router.get('/homepage',(req,res)=>{
     res.sendFile(path.resolve('pages/cehome.html'));
+});
+router.post('/give',(req,res)=>{
+    Csmodel.findOne({},(err,result)=>{
+        if(err)
+        {
+            console.log(err);
+        }
+        else
+        {
+            res.send(result);
+        }
+    })
 })
-
-
 
 module.exports=router;
